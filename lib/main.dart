10bbbaa0558309441f444fb3ad46f674db750763
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-05-14 17:05:40 +0200

    refactor main functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:roll_dice_app/my_gradient_container.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Text('Text'),
-  ));
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        //  backgroundColor: const Color.fromARGB(255, 94, 214, 46),
+        body: MyGradientContainer(),
+      ),
+    ),
+  );
 }
